5c99077c9575c8c708ac71a89994b05891f5887e
Fazal Ashraf <fazalashrafofficial@gmail.com>
2024-12-21 23:08:23 +0530

    added bullet shooting
    I learned about how to spawn bullet(using instantiate method ) from the tower and shoot towards the enemy (using rigidbody of the bullet gameobject )

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,14 +5,22 @@ public class Tower : MonoBehaviour
     public Transform towerHead;
     public Transform enemy;
 
-    public float attackRange = 3.0f;
+    public float attackRange = 3.0f;  // attack range of the tower
 
+    public GameObject bulletPrefab; // bullet prefab reference
+    public float bulletSpeed = 3.0f; // speed of the bullet
 
     private void Update()
     {
         if(Vector3.Distance(enemy.position, towerHead.position) < attackRange)
         {
             towerHead.LookAt(enemy);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject newBullet = Instantiate(bulletPrefab, towerHead.position, Quaternion.identity);
+                newBullet.GetComponent<Rigidbody>().velocity = (enemy.position - towerHead.position).normalized * bulletSpeed;
+            }
         }
     }
 
