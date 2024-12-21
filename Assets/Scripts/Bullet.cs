4be5cfd86e3605633cfad8e9216e766b8cfd6dbd
Fazal Ashraf <fazalashrafofficial@gmail.com>
2024-12-22 01:18:23 +0530

    added auto attack feature
    added auto attack feature to the tower so that whenever enemy enters the tower's attack range tower shoots  at the enemy, then while entering enemy's collider bullet will get destroyed (using trigger collider).

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            //Destroy(other.gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,8 +5,12 @@ public class Tower : MonoBehaviour
     public Transform towerHead;
     public Transform enemy;
 
+    [Header("Attack Details")]
     public float attackRange = 3.0f;  // attack range of the tower
+    public float attackCooldown = 2.0f;
+    private float lastAttackTime;
 
+    [Header("Bullet Details")]
     public GameObject bulletPrefab; // bullet prefab reference
     public float bulletSpeed = 3.0f; // speed of the bullet
 
@@ -16,12 +20,18 @@ public class Tower : MonoBehaviour
         {
             towerHead.LookAt(enemy);
 
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Time.time > lastAttackTime + attackCooldown)
             {
-                GameObject newBullet = Instantiate(bulletPrefab, towerHead.position, Quaternion.identity);
-                newBullet.GetComponent<Rigidbody>().velocity = (enemy.position - towerHead.position).normalized * bulletSpeed;
+                CreateBullet();
+                lastAttackTime = Time.time;
             }
         }
+    }
+
+    private void CreateBullet()
+    {
+        GameObject newBullet = Instantiate(bulletPrefab, towerHead.position, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody>().velocity = (enemy.position - towerHead.position).normalized * bulletSpeed;
     }
 
     private void OnDrawGizmos()
