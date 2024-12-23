750ba04e377cf084637b88d32b30bf6e767121cd
Fazal Ashraf <fazalashrafofficial@gmail.com>
2024-12-24 04:18:43 +0530

    added new feature to tower which will aim at random enemy in it's range

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
@@ -8,14 +9,56 @@ public class Tower : MonoBehaviour
     [SerializeField] private Transform towerHead;
     [SerializeField] private float rotationSpeed;
 
+    [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private LayerMask whatIsEnemy;
+
+
+    private void Start()
+    {
+        
+    }
+
+
     private void Update()
     {
-        if(currentEnemy == null) return;
+        if(currentEnemy == null)
+        {
+            currentEnemy = FindRandomEnemyWithInRange();
+            return;
+        }
+
+        if(Vector3.Distance(currentEnemy.position, towerHead.position) > attackRange)
+        {
+            currentEnemy = null;
+        }
+
         RotateTowardsEnemy();
+    }
+    private Transform FindRandomEnemyWithInRange()
+    {
+        List<Transform> possibleTargets = new List<Transform>();
+        Collider[] enemiesAround = Physics.OverlapSphere(transform.position, attackRange, whatIsEnemy);
+
+        foreach (var enemy in enemiesAround)
+        {
+            possibleTargets.Add(enemy.transform);
+        }
+
+        int randomIndex = Random.Range(0, possibleTargets.Count);
+
+        if(possibleTargets.Count <= 0)
+        {
+            return null;
+        }
+
+        return possibleTargets[randomIndex];
     }
 
     private void RotateTowardsEnemy()
     {
+        if (currentEnemy == null)
+            { return; }
+
         Vector3 directionToEnemy = currentEnemy.position - towerHead.position;
 
         Quaternion lookRotation = Quaternion.LookRotation(directionToEnemy);
@@ -23,5 +66,10 @@ public class Tower : MonoBehaviour
         Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, rotationSpeed).eulerAngles;
 
         towerHead.rotation = Quaternion.Euler(rotation);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
